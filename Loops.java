ee3c967ae062e7bf45360e27b0ef0d0cd8c7f732
Breno Rocha <51891048+Bre323@users.noreply.github.com>
2022-02-07 19:55:53 -0300

    Update Loops.java

--- Loops.java
+++ Loops.java
@@ -4,7 +4,7 @@ public class Loops {
 	
 	public static void main (String[] args) {
 		
-
+	int[] nums = {1, 2, 3, 4, 5};
 	int i, x, op;
 	i = x = op = 0;
 	Scanner scanner = new Scanner(System.in);
@@ -13,6 +13,7 @@ public class Loops {
 	System.out.println("1. While loop");
 	System.out.println("2. For loop");
 	System.out.println("3. Do While loop");
+	System.out.println("4. For Each loop");
 	op = scanner.nextInt();
 	
 	
@@ -68,12 +69,30 @@ public class Loops {
 
 	}
 	
+	
+	
+	if (op == 4) {
+		System.out.println("Precorrendo a variável 'nums'");
+		
+	/*
+	 * For each loop percorre todos os elementos de uma array/coleção de dados.
+	 */	
+		
+		for (int j : nums) {
+			System.out.println(j);
+		}
+	}
+	
+	
+	
 	/*
 	 * While: Executa infinitamente, caso contrário não executa.
 	 * 
 	 * For: Executa um determinado número de vezes.
 	 * 
 	 * Do While: Executa infinitamente, caso contrário executa apenas uma vez.
+	 * 
+	 * For Each: Executa o loop percorrendo os elementos de uma lista.
 	 */
 	
 	}
