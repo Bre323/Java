7ee024e4cd90667d5cc80b999b43ff3fcc8e6616
Breno Rocha <51891048+Bre323@users.noreply.github.com>
2022-02-08 12:46:47 -0300

    Add files via upload

---
+++ Final.java
@@ -0,0 +1,14 @@
+package programJar;
+
+public class Final {
+	public static void main(String[] args) {
+		
+		// 'final' é usado para que a variável declarada não tenha o seu valor alterado
+		// É uma boa prática a declaração de uma variável 'final' em UPPERCASE.
+		
+		final int X = 0;
+		final int Y = 1;
+		
+		System.out.printf("%d & %d", X, Y);
+	}
+}
