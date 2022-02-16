8b4ec8512e88108f09b8aa90f8454e1b815b9df9
Breno Rocha <51891048+Bre323@users.noreply.github.com>
2022-02-16 16:42:14 -0300

    Update Main.java

--- Encapsul/Main.java
+++ Encapsul/Main.java
@@ -4,23 +4,23 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		// Encapsulamento: Os atributos de uma classe ser„o privados ou protegidos e ser„o acessados por meio de getters e setters.
-		//				   Para proporcionar uma melhor seguranÁa, Atributos devem ser privados caso n„o exista motivo para deix·-lo p˙blico.
+		// Encapsulamento: Os atributos de uma classe ser√£o privados ou protegidos e ser√£o acessados por meio de getters e setters.
+		//				   Para proporcionar uma melhor seguran√ßa, Atributos devem ser privados caso n√£o exista motivo para deix√°-lo p√∫blico.
 		
-		// Getter: Os Getters s„o mÈtodos respons·veis pelo acesso dos atributos da classe que n„o est„o disponibilizados normalmente.
+		// Getter: Os Getters s√£o m√©todos respons√°veis pelo acesso dos atributos da classe que n√£o est√£o disponibilizados normalmente.
 		
-		// Setter: Os Setters s„o mÈtodos respons·veis pela modificaÁ„o dos atributos da classe que n„o est„o disponibilizados normalmente.
+		// Setter: Os Setters s√£o m√©todos respons√°veis pela modifica√ß√£o dos atributos da classe que n√£o est√£o disponibilizados normalmente.
 		
 		Caneta caneta = new Caneta("BIC", "Azul", 0.5f);
-		System.out.println(caneta.getModelo());
-		System.out.println(caneta.getCor());
-		System.out.println(caneta.getPonta());
+		Caneta bic = new Caneta(caneta);
+		
+		System.out.println(caneta.toString());
+		bic.setPonta(0);
+		System.out.println(bic.toString());
+		
 		caneta.setModelo("Zic");
 		caneta.setCor("Roxo");
 		caneta.setPonta(0.7f);
-		System.out.println(caneta.getModelo());
-		System.out.println(caneta.getCor());
-		System.out.println(caneta.getPonta());
+		System.out.println(caneta.toString());
 	}
-
 }
